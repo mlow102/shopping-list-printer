572e78956b39eb98dd1c637d49046d23974da85c
Matthew Low <matthewlow102@gmail.com>
2023-06-21 18:13:32 -0400

    Update print.py

--- src/print.py
+++ src/print.py
@@ -1,17 +1,9 @@
 from __future__ import print_function
 from escpos import printer
-
-import os
-import datetime
-import weather
 import reader
 
 # Sends a specified shopping list to the printer
 def main():    
-    year = datetime.date.today().year
-    month = datetime.date.today().month
-    day = datetime.date.today().day
-    timezone = '-04:00'
     p = printer.Usb(0x0416,0x5011,in_ep=0x81,out_ep=0x03)
     p.text("\n\n")
     p.set(align="center")    
